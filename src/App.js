2beeeb86a5c0b193925736934ab30acb97cc42f3
Eralp Orkun <eaorkun@gmail.com>
2022-01-02 13:32:48 -0600

    change back

--- src/App.js
+++ src/App.js
@@ -5,38 +5,38 @@ import ReactDOM from 'react-dom';
 import Button from '@mui/material/Button';
 
 
-<><head><meta name="viewport" content="initial-scale=1, width=device-width" /></head><>
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
-  <link
-    rel="stylesheet"
-    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
-</></>
+// <><head><meta name="viewport" content="initial-scale=1, width=device-width" /></head><>
+//   <link
+//     rel="stylesheet"
+//     href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+//   <link
+//     rel="stylesheet"
+//     href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+// </></>
 
 function App() {
 
-  return <Button variant="contained">Hello World</Button>;
+  // return <Button variant="contained">Hello World</Button>;
 
 
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit to <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit to <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
